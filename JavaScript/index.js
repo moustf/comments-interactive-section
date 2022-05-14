4134a19b9159f2ff68c042cd5278c703e227af88
moustf <tepro.18@gmail.com>
2022-05-14 19:20:17 +0300

    Created the functions which are responsible for adding the updated reply info to the local storage and call it when the page loaded close #22

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -312,6 +312,8 @@ setTimeout(() => {
   }
 }, 800);
 
+let textArr = [];
+
 // ? Added the event listener to the commentsReply div to listen to the edit div clicks and do the functionality.
 commentsReply.addEventListener("click", (e) => {
   if (
@@ -319,22 +321,83 @@ commentsReply.addEventListener("click", (e) => {
     e.target.alt === "edit icon" ||
     e.target.className === "edit-text"
   ) {
-    e.target
-      .closest(".edit-icon")
-      .nextElementSibling.setAttribute("contentEditable", "true");
-    e.target.closest(".edit-icon").nextElementSibling.focus();
-    e.target.closest(
-      ".edit-icon"
-    ).nextElementSibling.nextElementSibling.style.display = "block";
+    // ? When the event occurs, an element is added to the dom tree, so we need to check the order of the elements
+    if (
+      e.target.closest(".edit-icon").nextElementSibling.nextElementSibling
+        .localName == "button"
+    ) {
+      e.target.closest(
+        ".edit-icon"
+      ).nextElementSibling.nextElementSibling.style.display = "block";
+      e.target
+        .closest(".edit-icon")
+        .nextElementSibling.setAttribute("contentEditable", "true");
+      e.target.closest(".edit-icon").nextElementSibling.focus();
+    } else {
+      e.target.closest(
+        ".edit-icon"
+      ).nextElementSibling.nextElementSibling.nextElementSibling.style.display =
+        "block";
+      e.target
+        .closest(".edit-icon")
+        .nextElementSibling.nextElementSibling.setAttribute(
+          "contentEditable",
+          "true"
+        );
+      e.target
+        .closest(".edit-icon")
+        .nextElementSibling.nextElementSibling.focus();
+    }
   }
 });
 
 // ? Added the event listener to the commentsReply div to listen to the update button clicks and do the functionality.
 commentsReply.addEventListener("click", (e) => {
-  if ((e.target.className == "update-btn")) {
+  if (e.target.className == "update-btn") {
     e.target.previousElementSibling.setAttribute("contentEditable", "false");
     e.target.previousElementSibling.blur();
     e.target.style.display = "none";
-    
+
+    let text = e.target.previousElementSibling.textContent;
+    let id = e.target.closest(".reply").dataset.reply;
+    setReplyToLS(text, id);
   }
 });
+
+// ? creating the function which is responsible for adding the text to the local storage.
+function setReplyToLS(text, id) {
+  let isNotThere = false;
+  if (textArr.length === 0) {
+    textArr.push({ "data-reply": id, text: text });
+    window.localStorage.setItem("text", JSON.stringify(textArr));
+  } else {
+    for (let i = 0; i < textArr.length; i++) {
+      if (textArr[i]["data-reply"] == id) {
+        textArr[i].text = text;
+        window.localStorage.setItem("text", JSON.stringify(textArr));
+        isNotThere = true;
+      }
+    }
+    if (isNotThere === false) {
+      textArr.push({ "data-reply": id, text: text });
+      window.localStorage.setItem("text", JSON.stringify(textArr));
+    }
+  }
+}
+
+// ? creating the function which is responsible for taking the replies data from the local storage. 
+function addToPageFormLS() {
+  let textArr = JSON.parse(localStorage.getItem("text"));
+  textArr.forEach((obj) => {
+    document
+      .querySelector(`[data-reply="${obj["data-reply"]}"]`)
+      .querySelector(".comment-text").textContent = obj.text;
+  });
+}
+
+// ? Set a time out to make the check happens after the fetch finishs.
+setTimeout(() => {
+  if (localStorage.getItem("text")) {
+    addToPageFormLS();
+  }
+}, 500);
